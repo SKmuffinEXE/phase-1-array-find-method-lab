e2ce48e14250c7d9e317a7cc47b403726637b565
David <DEvangelista54@gmail.com>
2021-09-01 22:39:42 -0400

    commit tree ab0aaf01dd42e63d4150b6ce4e0011650797dbc5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+function superbowlWin(record){
+    // return record.result === "W";
+    const foundWin = record.find((element)=>{
+        if (element.result === "W"){
+            return element;
+            }        
+        })
+        console.log("Hi")
+        console.log(foundWin)
+    if(foundWin !== undefined){
+        return foundWin.year
+    }
+    else{
+        return foundWin
+    }
+}
+
+// record.find(superbowlWin)["year"]
